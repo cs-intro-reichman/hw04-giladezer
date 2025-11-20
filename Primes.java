5e36c2577bdd5669a3373d03bfbfea1cc488b2ac
Gilad Ezer <gilad.ezer@post.runi.ac.il>
2025-11-20 12:58:19 +0200

    done

--- Primes.java
+++ Primes.java
@@ -14,6 +14,7 @@ public class Primes {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         int count = 0;
+        System.out.println("Prime numbers up to " + n + ":");
         for (int i = 2; i <= n; i++) {
             if (isPrime(i)) {
                 count ++;
