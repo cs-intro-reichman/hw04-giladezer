b9377e865a5dde54a0b2138cb7c1169d855aff80
Gilad Ezer <gilad.ezer@post.runi.ac.il>
2025-11-20 12:54:19 +0200

    done

--- Primes.java
+++ Primes.java
@@ -13,10 +13,12 @@ public class Primes {
     }
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
+        int count = 0;
         for (int i = 2; i <= n; i++) {
             if (isPrime(i)) {
-                System.out.println(i);
+                count ++;
             }
         }
+       System.out.println( "There are " + count + " primes between 2 and 7");
     }
 }
